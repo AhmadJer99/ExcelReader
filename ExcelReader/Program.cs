7108aa400dadb942de642e452447f50e9f61f61b
Ahmad Jerjees <ajerjees010@gmail.com>
2025-05-11 13:21:17 +0300

    Add project files.

---
+++ ExcelReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseSerilog()
+    .ConfigureServices((context, services) =>
+    {
+        // Register your services here
+    })
+    .Build();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+           .AddEnvironmentVariables();
+}
